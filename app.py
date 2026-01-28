6c0f6f802b68ee67facbf5b58fda3d32feb056c1
dudu <eduardo.cereza@outlook.com>
2026-01-28 12:00:48 -0300

    funcoes criar e editar

--- app.py
+++ app.py
@@ -2,6 +2,67 @@ import os
 import time
 
 os.system('cls')
+def reset(): 
+    time.sleep(1)
+    os.system('cls')
+
+def pausa(msg="\nDeseja voltar? Aperte ENTER..."):
+    input(msg)
+
+mostrarMenu = True
+
+contatos = []
+
+
+def salvar(contato):
+    contatos.append(contato)
+    reset()
+    print("SALVO COM SUCESSO!")
+
+def editar():
+    reset()
+
+    i = 0
+    print("CONTATOS: \n")
+    for contato in contatos:
+        nome = contato["name"]
+        print(f"{i}. {nome}")
+        i += 1
+
+    cnt = int (input("\nQUAL CONTANTO VC QUER EDITAR? DIGITE O NUMERO: "))
+
+
+    repetir = True
+
+    if(repetir):
+        print("\n[ 1 ] NOME")
+        print("[ 2 ] TELEFONE")
+        print("[ 3 ] EMAIL")
+        opt = int (input("\nDIGITE A OPCAO QUE QUER EDITAR: "))
+
+        if(opt == 1):
+            name = input("DIGITE UM NOVO NOME: ")
+            contatos[cnt]["name"] = name
+            repetir = False
+        elif(opt == 2):
+            telefone = input("DIGITE UM NOVO TELEFONE: ")
+            contatos[cnt]["telefone"] = telefone
+            repetir = False
+
+        elif(opt == 3):
+            email = input("DIGITE UM NOVO EMAIL: ")
+            contatos[cnt]["email"] = email
+            repetir = False
+        else:
+            print("DIGITE UMA OPCAO VÁLIDA!")
+            time.sleep(2)
+            os.system('cls')
+            repetir = True
+    else:
+        pausa()
+        mostrarMenu = True
+
+
 
 #Lista de opcoes
 options = [
@@ -9,7 +70,8 @@ options = [
     "[ 2 ] EDITAR",
     "[ 3 ] DELETAR",
     "[ 4 ] MARCA COMO FAVORITO",
-    "[ 5 ] SAIR"
+    "[ 5 ] VER USUÁRIOS CADASTRADOS",
+    "[ 6 ] SAIR"
     ]
 
 
@@ -17,29 +79,59 @@ def mostrar_opcoes():
     print("\nEscolha uma das opcoes abaixo:\n")
     for option in options:
         print(option)
+
+def criar_contato():
+    name = input("DIGITE O NOME: ")
+    telefone = input("DIGITE O TELEGONE: ")
+    email = input("DIGITE O EMAIL: ")
     
+    contato = {
+        "name": name,
+        "telefone": telefone,
+        "email": email,
+    }
+    return contato
+
+
+
 while True:
-    mostrar_opcoes()
+    if mostrarMenu:
+        reset()
+        mostrar_opcoes()
+        mostrar_menu = False 
+
     resposta = int (input("\nEscolha uma opcao: "))
 
     match resposta:
         case 1: 
-            print("SALVAR")
+            os.system('cls')
+            salvar(criar_contato())
+            
         case 2: 
-            print("EDITAR")
+            editar()
         case 3: 
             print("DELETAR")
         case 4: 
             print("MARCA COMO FAVORITO")
-        case 5:
+        case 5: 
+            reset()
+            i = 1
+            print("CONTATOS\n")
+            for contato in contatos:
+                nome = contato["name"]
+                email = contato["email"]
+                telefone = contato["telefone"]
+                print(f"{i}. {nome}")
+                print(f"    TELEFONE: {telefone}")
+                print(f"    EMAIL: {email}")
+                i += 1
+            pausa()              
+            mostrar_menu = True  
+        case 6:
             print("SAIR")
             os.system('cls')
             break
         case _: 
-            print("\nESCOLHA UMA OPCAO VÁLIDA")
-            time.sleep(1)
-            os.system('cls')
-            mostrar_opcoes()
+            reset()
 
-    time.sleep(1)
-    os.system('cls')
+
